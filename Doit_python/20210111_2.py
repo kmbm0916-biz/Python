fefdbb7f91feae9c5c0b126cc371bc9fe851d8f3
BM0916 <46841126+BM0916@users.noreply.github.com>
2021-01-19 15:06:55 +0900

    2021-01-19_1

---
+++ Doit_python/20210111_2.py
@@ -0,0 +1,83 @@
+# 리스트 더하기
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# print(a + b)
+
+# 리스트 반복하기
+# a = [1, 2, 3]
+# print(a * 3)
+
+# 리스트 하나의 값 수정
+# a = [1, 2, 3]
+# a[2] = 4
+# print(a)
+
+# 리스트에서 연속된 범위의 값 수정
+# a = [1, 2, 3]
+# print(a[1:2])
+# a[1:2] = ['a', 'b', 'c']
+# print(a)
+
+# [] 사용해 리스트 요소 삭제하기
+# a = [1, 2, 3]
+# a[1:3] = []
+# print(a)
+
+# del 함수 사용해 리스트 요소 삭제
+# a = [1, 'c', 4]
+# del a[1]
+# print(a)
+
+# 리스트에 요소 추가
+# a = [1, 2, 3]
+# a.append(4)
+# print(a)
+
+# 리스트 정렬(sort)
+# a = [1, 4, 3, 2]
+# a.sort()
+# print(a)
+# + 문자 정렬
+# a = ['a', 'c', 'b']
+# a.sort()
+# print(a)
+
+# 리스트 뒤집기(reverse)
+# a = ['a', 'c', 'b']
+# a.reverse()
+# print(a)
+
+# 위치 반환(index)
+# a = [1, 2, 3]
+# print(a.index(3))
+# print(a.index(1))
+
+# 리스트에 요소 삽입(insert)
+# a = [1, 2, 3]
+# a.insert(0, 4)
+# print(a)
+
+# 리스트 요소 제거(remove)
+# a = [1, 2, 3, 1, 2, 3]
+# a.remove(3)
+# print(a)
+
+# 리스트 요소 끄집어내기(pop)
+# a = [1, 2, 3]
+# a.pop()
+# print(a)
+
+# 리스트에 포함된 요소 x의 개수 세기(count)
+# a = [1, 2, 3, 1]
+# a.count(1)
+# print(a)
+
+# 리스트 확장(extend)
+# a = [1, 2, 3]
+# a.extend([4, 5])
+# print(a)
+# b = [6, 7]
+# a.extend(b)
+# print(a)
+
+# 
